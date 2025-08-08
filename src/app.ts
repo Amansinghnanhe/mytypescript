f57b4e7a3af2ed241562d8ae13552b57f566089f
Amansinghnanhe <139671857+Amansinghnanhe@users.noreply.github.com>
2025-08-08 11:21:33 +0530

    Reorganize app.ts by removing commented-out code for improved readability

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-// import express from 'express';
-// import userRoutes from './routes/user.routes';
-// import dotenv from 'dotenv';
-// dotenv.config();
+import express from 'express';
+import userRoutes from './routes/user.routes';
+import dotenv from 'dotenv';
+dotenv.config();
 
-// const app = express();
-// app.use(express.json());
+const app = express();
+app.use(express.json());
 
-// app.use('/api/users', userRoutes);
+app.use('/api/users', userRoutes);
 
-// export default app;
+export default app;
